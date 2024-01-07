7629df948341e3fb780acbf2fb571ef6594b53a8
muhammadzidanfadillah <115553474+muhammadzidanfadilah@users.noreply.github.com>
2024-01-07 04:44:46 -0800

    Delete Penjualan-kue-master directory

--- Penjualan-kue-master/admin/footer.php
+++
@@ -1,5 +0,0 @@
-<footer style="background-color: gray;padding: 1px;" class="print">
-	<h5 class="text-center">Copyright&copy; Muhammad Zidan Fadillah</h5>
-</footer>
-</body>
-</html>
--- Penjualan-kue-master/koneksi/koneksi.php
+++
@@ -1,3 +0,0 @@
-<?php 
-$conn = mysqli_connect("localhost", "root", "", "dbpw192_18410100054");
- ?>
